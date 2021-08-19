d5cfe3c33ef86bb0848f46ac9538286b3f77f6e9
rajkumarjakkula <rajkumarjakkula969@gmail.com>
2021-08-19 20:03:48 +0530

    Requirement 1: FizzBuzz(divisible by 3 or divisible by 5)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,13 @@
 public class FizzBuzz {
+
+    public String FizzBuzzFunction(int value) {
+        if(value % 5 == 0){
+            return "Buzz";
+        }
+        if(value % 3 == 0){
+            return "Fizz";
+        }
+
+        return Integer.toString(value);
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    public FizzBuzz fizzBuzz=null;
+    @BeforeEach
+    void CreateObject(){
+        fizzBuzz=new FizzBuzz();
+    }
+
+    @Test
+    void onNumberDivisibleByThreeReturnFizz()
+    {
+        String actualValue = fizzBuzz.FizzBuzzFunction(36);
+        String expectedValue = "Fizz";
+        assertEquals(actualValue,expectedValue);
+    }
+
+    @Test
+    void  onNumberDivisibleByFiveReturnBuzz()
+    {
+        String actualValue = fizzBuzz.FizzBuzzFunction(35);
+        String expectedValue = "Buzz";
+        assertEquals(actualValue,expectedValue);
+    }
+
+    @Test
+    void  onNumberNotDivisibleByFiveAndThreeReturnNumber()
+    {
+        String actualValue = fizzBuzz.FizzBuzzFunction(32);
+        String expectedValue = Integer.toString(32);
+        assertEquals(actualValue,expectedValue);
+    }
+
 }
